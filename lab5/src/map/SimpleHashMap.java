df3c367725195cf2660d37ec9d533ca605bc734c
Johan Baversjo <baversjo@gmail.com>
2012-02-22 13:02:05 +0100

    Lab5 show and linkiterator

--- lab5/src/map/SimpleHashMap.java
+++ lab5/src/map/SimpleHashMap.java
@@ -1,5 +1,7 @@
 package map;
 
+import java.util.Iterator;
+
 public class SimpleHashMap<K,V> implements Map<K,V> {
 	
 	public static final int INITIAL_CAPACITY = 16;
@@ -39,10 +41,47 @@ public class SimpleHashMap<K,V> implements Map<K,V> {
 		return 0;
 	}
 	
+	/*
+	public String show(){
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i < table.length; i++){
+			sb.append(i);
+			sb.append("\t");
+			Entry<K,V> e = table[i];
+			if(e )
+			sb.append(table[i].toString());
+		}
+		return sb.toString();
+	}
+	*/
+	
+	private class LinkIterator implements Iterator<Entry>{
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public Entry next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 	private static class Entry<K,V> implements Map.Entry<K,V> {
 		
 		private K key;
 		private V value;
+		private Entry<K,V> next;
 		
 		public Entry(K key, V value) {
 			this.key = key;
